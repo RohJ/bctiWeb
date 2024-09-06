699cd2c48d02ae106f438c0c6f5617c6d499349b
RohJ <rohitsjadhav@gmail.com>
2024-09-07 01:03:29 +0530

    Home page progress

--- src/app/website/home/home.component.ts
+++ src/app/website/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Glide from '@glidejs/glide';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 var glide = new Glide('.text_testimony', {
   type: 'carousel',
@@ -7,6 +8,28 @@ var glide = new Glide('.text_testimony', {
   autoplay: 4000,
   perView: 1
 })
+
+var glide1 = new Glide('.Counseling', {
+  type: 'carousel',
+  hoverpause: true,
+  autoplay: 4000,
+  perView: 1
+})
+
+var glide2 = new Glide('.Training', {
+  type: 'carousel',
+  hoverpause: true,
+  autoplay: 4000,
+  perView: 1
+})
+
+var glide3 = new Glide('.Courses', {
+  type: 'carousel',
+  hoverpause: true,
+  autoplay: 4000,
+  perView: 1
+})
+
 
 @Component({
   selector: 'app-home',
@@ -18,5 +41,8 @@ var glide = new Glide('.text_testimony', {
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
     glide.mount();
+    glide1.mount();
+    glide2.mount();
+    glide3.mount();
   }
 }
