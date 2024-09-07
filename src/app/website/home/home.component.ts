60e070886ea8afe93f797f6dc49210322556e1c8
RohJ <rohitsjadhav@gmail.com>
2024-09-07 11:29:38 +0530

    home page updated

--- src/app/website/home/home.component.ts
+++ src/app/website/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import Glide from '@glidejs/glide';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 var glide = new Glide('.text_testimony', {
   type: 'carousel',
@@ -39,10 +38,20 @@ var glide3 = new Glide('.Courses', {
 
 
 export class HomeComponent implements OnInit {
+
   ngOnInit(): void {
     glide.mount();
     glide1.mount();
     glide2.mount();
     glide3.mount();
+
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('custom-class');
   }
+
+  ngOnDestroy() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('custom-class');
+  }
+
 }
