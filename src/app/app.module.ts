4aa2abb44f08318c4390cc9869308b23b669332e
RohJ <rohitsjadhav@gmail.com>
2024-10-26 16:27:08 +0530

    Home Page Layout complete
    Footer completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  { ReactiveFormsModule }  from  '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './website/shared/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './website/home/home.component';
+import { FooterComponent } from "./website/shared/footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
-  ],
+    NgbModule,
+    ReactiveFormsModule,
+],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/website/home/home.component.ts
+++ src/app/website/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import Glide from '@glidejs/glide';
 
 var glide = new Glide('.text_testimony', {
@@ -39,6 +40,19 @@ var glide3 = new Glide('.Courses', {
 
 export class HomeComponent implements OnInit {
 
+
+  contactForm: FormGroup = new FormGroup({
+    fullname: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+
   ngOnInit(): void {
     glide.mount();
     glide1.mount();
@@ -47,11 +61,49 @@ export class HomeComponent implements OnInit {
 
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('custom-class');
+
+    this.contactForm = this.formBuilder.group(
+      {
+        fullname: [null, [Validators.required]],
+        phone: [null, [Validators.required, Validators.pattern("^[0-9]{10}$")]],
+        email: [null, [Validators.required, Validators.email]],
+        message: [null, [Validators.required]]
+
+      }
+    );
+  }
+
+    get f(): { [key: string]: AbstractControl } {
+    return this.contactForm.controls;
+
   }
 
   ngOnDestroy() {
     var body = document.getElementsByTagName('body')[0];
     body.classList.remove('custom-class');
+  }
+
+  // createContactForm() {
+  //   this.contactForm = this.formBuilder.group({
+  //     fullName: [''],
+  //     email: [''],
+  //     message: ['']
+  //   });
+  // }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+    console.log(this.contactForm);
+    console.log(JSON.stringify(this.contactForm.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.contactForm.reset();
   }
 
 }
